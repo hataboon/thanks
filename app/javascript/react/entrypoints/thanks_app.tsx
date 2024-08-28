5da2484b0bb259b17057aa1a2d825c16a93d6302
hataboon <straightener0201@hotmail.co.jp>
2024-08-28 11:48:29 +0900

    Normalize line endings to CRLF

---
+++ app/javascript/react/entrypoints/thanks_app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client"; //createRoot: React 18から導入された新しいAPIで、Reactアプリケーションのルートを作成します。
+import Thanks from "../features/thanks";
+
+const container = document.getElementById("thanks_app");
+if (container) { // containerが存在する場合のみ、以下のコードを実行します。
+  const root = createRoot(container); // 取得した要素にReactアプリケーションのルートを作成します。
+  root.render(<Thanks />); // Thanksコンポーネントをレンダリング（表示）します。
+}
---
+++ app/javascript/react/features/thanks/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Thanks() { // Thanksという名前の関数コンポーネントを定義します。
+  return (
+    <>
+      helloworld
+    </>
+  );
+}
