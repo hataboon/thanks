79aa88ce9e10cc9f457d74f015a30426dfcd4ffb
hataboon <straightener0201@hotmail.co.jp>
2024-09-02 12:48:31 +0900

    create delete add

--- app/javascript/react/components/DiaryEntry.tsx
+++ app/javascript/react/components/DiaryEntry.tsx
@@ -9,21 +9,26 @@ interface DiaryEntryProps {
 
 const DiaryEntry: React.FC<DiaryEntryProps> = ({ entry, onEdit, onDelete }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-6 text-center">
+    <div className="bg-white rounded-lg shadow-md p-6 relative">
       <p className="font-bold text-lg mb-2">{entry.category}</p>
       <p className="mb-4">{entry.content}</p>
       <p className="text-sm text-gray-500">{entry.date}</p>
-      <div className="absolute bottom-2 right-2 flex space-x-2">
+      <div className="absolute bottom-2 right-2 flex space-x-2 mt-4">
       <button
         onClick={() => onEdit(entry)}
-        className="text-blue-500 hover:text-blue-700"
+        className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-sm font-sans text-blue-700 ring-1 ring-inset ring-blue-700/10"
       >
         編集
         </button>
+        {/*削除ボタン*/}
+        <button
+          onClick={() => onDelete(entry.id)}
+          className="text-red-500 hover:text-red-700"
+        >
+          削除
+        </button>
       </div>
     </div>
-     
-        
   );
 };
 
