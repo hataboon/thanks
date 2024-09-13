d41c54c7e903fbccb23ee34e6ee620f62d864391
hataboon <straightener0201@hotmail.co.jp>
2024-09-13 21:51:03 +0900

    ヘッダー修正

--- test/controllers/thanks_controller_test.rb
+++ test/controllers/thanks_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class ThanksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)  # テストユーザーをセット
+    sign_in @user        # テストユーザーでログイン
+  end
+
   test "should get index" do
-    get thanks_index_url
+    get thanks_url   # thanks_index_urlではなくthanks_urlに変更
     assert_response :success
   end
 end
